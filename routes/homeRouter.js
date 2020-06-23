cc0f05340063c552b93691474d0d60974af31912
Christian Pari <ChristianPari@outlook.com>
2020-06-22 22:03:26 -0400

    removed unused code

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/home', (req, res) => {
 
     const songsLink = `http://localhost:${process.env.PORT}/songs`,
         albumsLink = '',

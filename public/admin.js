1bbc356e35fe8445af27ea86b985645cbe719560
Christian Pari <ChristianPari@outlook.com>
2020-06-23 20:43:31 -0400

    Song collection manipulation completed

--- public/admin.js
+++ public/admin.js
@@ -1,6 +1,8 @@
 window.onload = () => {
 
     document.getElementById('newSong').onclick = createNewSong;
+    document.getElementById('removeSong').onclick = deleteSong;
+    document.getElementById('updateSong').onclick = songUpdate;
 
     for (const input of document.getElementsByTagName('input')) {
 
@@ -18,11 +20,13 @@ function createNewSong() {
 
     inputs.forEach(input => {
 
-        reqBody[input.id] = input.value;
+        if (input.value != '') { reqBody[input.className] = input.value; }
 
     });
 
-    const endpoint = `${window.location}post`,
+    if (Object.keys(reqBody).length != 7) { return alert('All fields are required') };
+
+    const endpoint = `${window.location}/newSong`,
         reqObj = {
 
             headers: {
@@ -37,6 +41,91 @@ function createNewSong() {
 
         };
 
-    // create fetch
+    fetch(endpoint, reqObj)
+        .then(rs => { return rs.json(); })
+        .then(res => { console.log(res); })
+        .catch(err => { console.log(err); })
+
+    inputs.forEach(input => {
+
+        input.value = '';
+
+    });
+
+};
+
+// update song function
+
+function deleteSong() {
+
+    const songID = document.getElementById('delSongID').value;
+    songIdInput = document.getElementById('delSongID');
+
+    if (songID == '') { return alert('Must enter a song ID'); };
+
+    const endpoint = `${window.location}/deleteSong/${songID}`,
+        reqObj = {
+
+            method: 'DELETE'
+
+        };
+
+    fetch(endpoint, reqObj)
+        .then(rs => { return rs.json(); })
+        .then(res => { console.log(res); })
+        .catch(err => { console.log(err); })
+
+    songIdInput.value = '';
+
+};
+
+function songUpdate() {
+
+    const songID = document.getElementById('upSongID').value,
+        songIdInput = document.getElementById('upSongID');
+
+
+    if (songID == '') { return alert("Must Enter a Valid Song ID"); };
+
+    const elements = document.getElementById('songPatch').childNodes,
+        inputs = Array.from({ length: elements.length - 3 }, (a, b) => { if (elements[b + 2].nodeName.toLowerCase() == 'input') { return elements[b + 2]; } }),
+        reqBody = {};
+
+    inputs.forEach(input => {
+
+        if (input.value != '') { reqBody[input.className] = input.value; }
+
+    });
+
+    if (Object.keys(reqBody).length < 1) { return alert("Must include at least one field to update the song document"); };
+
+    const endpoint = `${window.location}/updateSong/${songID}`,
+        reqObj = {
+
+            headers: {
+
+                'Access-Control-Allow-Origin': '*',
+                Accept: 'application/json',
+                'content-type': 'application/json'
+
+            },
+
+            method: 'PATCH',
+            body: JSON.stringify(reqBody)
+
+        };
+
+    fetch(endpoint, reqObj)
+        .then(rs => { return rs.json(); })
+        .then(res => { console.log(res); })
+        .catch(err => { console.log(err); })
+
+    songIdInput.value = '';
+
+    inputs.forEach(input => {
+
+        input.value = '';
+
+    });
 
 };
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -9,7 +9,7 @@ router.get('/:user_key', adminAuth, (req, res) => {
 
 });
 
-router.post('/:user_key/post', adminAuth, async(req, res) => {
+router.post('/:user_key/newSong', adminAuth, async(req, res) => {
 
     try {
 
@@ -23,7 +23,7 @@ router.post('/:user_key/post', adminAuth, async(req, res) => {
             message: "Successful Creation of New Song",
             new_song: newSong
 
-        })
+        });
 
     } catch (err) {
 
@@ -33,7 +33,71 @@ router.post('/:user_key/post', adminAuth, async(req, res) => {
             message: err.message,
             error: err
 
-        })
+        });
+
+    };
+
+});
+
+router.delete('/:user_key/deleteSong/:id', adminAuth, async(req, res) => {
+
+    try {
+
+        const songID = req.params.id,
+            removedSong = await Song.findOne({ '_id': songID });
+
+        await Song.findOneAndDelete({ '_id': songID });
+
+        return res.status(200).json({
+
+            status: 200,
+            message: "Successful Song Deletion",
+            deleted_song: removedSong
+
+        });
+
+    } catch (err) {
+
+        return res.status(500).json({
+
+            status: 500,
+            message: err.message,
+            error: err
+
+        });
+
+    };
+
+});
+
+router.patch('/:user_key/updateSong/:id', adminAuth, async(req, res) => {
+
+    try {
+
+        const songID = req.params.id,
+            oldSong = await Song.findById(songID),
+            newData = req.body;
+
+        const newSong = await Song.findOneAndUpdate({ '_id': songID }, newData);
+
+        return res.status(200).json({
+
+            status: 200,
+            message: "Successful Song Update",
+            new_song: newSong,
+            old_song: oldSong
+
+        });
+
+    } catch (err) {
+
+        return res.status(500).json({
+
+            status: 500,
+            message: err.message,
+            error: err
+
+        });
 
     };
 
